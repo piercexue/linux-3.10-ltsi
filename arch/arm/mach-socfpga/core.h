d7e8cb0fb1d2e19561a650b6ac1713371f268238
Dinh Nguyen <dinguyen@altera.com>
2012-10-18 13:39:04 -0600

    FogBugz #80434: Bring all peripherals out of reset
    Write a 0 to the PERMODRST register in the HPS reset manager. This
    releases all of the HPS peripherals from reset.
    
    Signed-off-by: Dinh Nguyen <dinguyen@altera.com>

--- arch/arm/mach-socfpga/core.h
+++ arch/arm/mach-socfpga/core.h
@@ -20,15 +20,17 @@
 #ifndef __MACH_CORE_H
 #define __MACH_CORE_H
 
+#define SOCFPGA_MODPERRST	0x14
+
 extern void secondary_startup(void);
 extern void __iomem *socfpga_scu_base_addr;
+extern void __iomem *sys_manager_base_addr;
+extern void __iomem *rst_manager_base_addr;
 
+extern void socfpga_secondary_startup(void);
+extern void socfpga_cpu_die(unsigned int cpu);
 extern void socfpga_init_clocks(void);
-extern void socfpga_sysmgr_init(void);
 
-extern struct smp_operations socfpga_smp_ops;
-extern char secondary_trampoline, secondary_trampoline_end;
+extern struct dw_mci_board sdmmc_platform_data;
 
-#define SOCFPGA_SCU_VIRT_BASE   0xfffec000
-
-#endif
+#endif /* __CORE_H */
