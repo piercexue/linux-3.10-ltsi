4de7f6ea5efc596fa5d02938b89ea6a090e955e6
Dinh Nguyen <dinguyen@altera.com>
2013-01-21 15:54:31 -0600

    FogBugz #95886: Add reset code to SOCFPGA
    Issue a cold or warm reset to the HW.
    
    Signed-off-by: Dinh Nguyen <dinguyen@altera.com>

--- arch/arm/mach-socfpga/core.h
+++ arch/arm/mach-socfpga/core.h
@@ -1,8 +1,13 @@
 #ifndef __CORE_H
 #define __CORE_H
 
-#define SOCFPGA_MODPERRST	0x14
-#define SOCFPGA_BRGMODRST	0x1c
+#define SOCFPGA_RSTMGR_CTRL	0x04
+#define SOCFPGA_RSTMGR_MODPERRST	0x14
+#define SOCFPGA_RSTMGR_BRGMODRST	0x1c
+
+/* System Manager bits */
+#define RSTMGR_CTRL_SWCOLDRSTREQ	0x1	/* Cold Reset */
+#define RSTMGR_CTRL_SWWARMRSTREQ	0x2	/* Warm Reset */
 
 extern void secondary_startup(void);
 extern void __iomem *socfpga_scu_base_addr;
