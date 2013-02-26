79ea523c4c543131e3981b9ab623a7e7822f1e7d
Dinh Nguyen <dinguyen@altera.com>
2013-02-26 14:08:00 -0600

    FogBugz #102825: Add cpu1-start-addr to device tree implementation
    Convert the usage of CPU1 start address to a device tree implementation.
    Also, clean up the unnecessary defines for multiple boards in the
    altera_dt_match table.
    "altr, socfpga" is all we need in the socfpga.c file.
    
    Add documentation for the Altera's reset and system manager bindings.
    
    Signed-off-by: Dinh Nguyen <dinguyen@altera.com>

--- arch/arm/mach-socfpga/socfpga.c
+++ arch/arm/mach-socfpga/socfpga.c
@@ -84,16 +84,6 @@ static void __init socfpga_scu_map_io(void)
 	iotable_init(&scu_io_desc, 1);
 }
 
-static void __init init_socfpga_vt(void)
-{
-	cpu1start_addr = 0xffd08010;
-}
-
-static void __init init_socfpga(void)
-{
-	cpu1start_addr = 0xffd080c4;
-}
-
 static void __init enable_periphs(void)
 {
 	/* Release all peripherals from reset.*/
@@ -218,6 +208,11 @@ static void __init socfpga_sysmgr_init(void)
 	struct device_node *np;
 
 	np = of_find_compatible_node(NULL, NULL, "altr,sys-mgr");
+
+	if (of_property_read_u32(np, "cpu1-start-addr",
+			(u32 *) &cpu1start_addr))
+		pr_err("SMP: Need cpu1-start-addr in device tree.\n");
+
 	sys_manager_base_addr = of_iomap(np, 0);
 
 	np = of_find_compatible_node(NULL, NULL, "altr,rst-mgr");
@@ -233,11 +228,6 @@ static void __init gic_init_irq(void)
 {
 	of_irq_init(irq_match);
 	socfpga_sysmgr_init();
-
-	if (of_machine_is_compatible("altr,socfpga-vt"))
-		init_socfpga_vt();
-	else
-		init_socfpga();
 
 	socfpga_init_clocks();
 	twd_local_timer_of_register();
@@ -272,9 +262,6 @@ static void __init socfpga_cyclone5_init(void)
 
 static const char *altera_dt_match[] = {
 	"altr,socfpga",
-	"altr,socfpga-cyclone5",
-	"altr,socfpga-vt",
-	"altr,socfpga-ice",
 	NULL
 };
 
