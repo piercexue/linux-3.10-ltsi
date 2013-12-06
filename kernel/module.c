49f4a7953e1c855e40f04d0d9da7e2147b1d3c19
Yves Vandervennet <yvanderv@altera.com>
2013-12-06 12:39:36 -0600

    GCOV fix for GCC 4.7.x
    Changes the data structures of GCOV to handle GCC 4.7 code
    
    Signed-off-by: Yves Vandervennet <yvanderv@altera.com>

--- kernel/module.c
+++ kernel/module.c
@@ -3003,6 +3003,9 @@ static void do_mod_ctors(struct module *mod)
 
 	for (i = 0; i < mod->num_ctors; i++)
 		mod->ctors[i]();
+
+	for (i = 0; i < mod->num_init_array; i++)
+		mod->init_array[i]();
 #endif
 }
 
